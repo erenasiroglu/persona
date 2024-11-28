97c170eb4beb393a9a88bf9179cbf5545be8752d
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-11-28 22:49:06 +0300

    fix: deployment error

--- app/context/StepContext.tsx
+++ app/context/StepContext.tsx
@@ -105,3 +105,11 @@ export function useStepContext() {
   }
   return context;
 }
+
+export function useStep() {
+  const context = useContext(StepContext);
+  if (context === undefined) {
+    throw new Error("useStep must be used within a StepProvider");
+  }
+  return context;
+}
